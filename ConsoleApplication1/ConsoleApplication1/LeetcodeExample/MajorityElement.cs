fb07434eec2b93f8fce9817731ea96bf911cdeaa
maggylin_acer <maggylin0317@gmail.com>
2022-04-29 21:35:38 +0800

    update

---
+++ ConsoleApplication1/ConsoleApplication1/LeetcodeExample/MajorityElement.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1.LeetcodeExample
+{
+    class MajorityElement
+    {
+
+        public int MajorityElementEx(int[] nums)
+        {
+            // more than [n / 2] times : 中間一定會是最多次的數字
+            Array.Sort(nums);
+            int mid = nums.Length / 2;
+            return nums[mid];
+        }
+
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/LeetcodeExample/MoveZeroes.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1.LeetcodeExample
+{
+    //https://leetcode.com/problems/move-zeroes/
+    class MoveZeroes
+    {
+        //Note that you must do this in-place without making a copy of the array.
+
+        //先把非0的都放入原本array,剩下都補0
+        public void MoveZeroesEx1(int[] nums)
+        {
+            int i = 0; int j = 0;
+            for (i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != 0)
+                {
+                    nums[j] = nums[i];
+                    j++;
+                }
+            }
+
+            for (int k = j; k < nums.Length; k++)
+            {
+                nums[k] = 0;
+            }
+
+        }
+
+        //參考 : https://ithelp.ithome.com.tw/articles/10221042
+        //先用pointer 紀錄第一個0位置
+        //nums[i]非0 且 i != pointer時,交換位置
+        public void MoveZeroesEx2(int[] nums)
+        {
+            int pointer = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != 0)
+                {
+                    if (pointer != i)
+                    {
+                        nums[pointer] = nums[i];
+                        nums[i] = 0;
+                    }
+                    pointer++;
+                }
+            }
+
+        }
+
+    }
+}
