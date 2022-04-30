6e1452825cb2e464819058e2a411a5b0af0f11c6
maggylin_acer <maggylin0317@gmail.com>
2022-04-30 23:43:16 +0800

    update

---
+++ ConsoleApplication1/ConsoleApplication1/LeetcodeExample/PalindromeLinkedList.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1.LeetcodeExample
+{
+    class PalindromeLinkedList
+    {
+        //建立 正 與 反 字串 , 比對 2 字串有沒有相同
+        // Time Limit Exceeded...
+        public bool IsPalindromeEx1(ListNode head)
+        {
+            string front = "";
+            string back = "";
+
+            while(head != null)
+            {
+                front = front + head.val;
+                back = head.val + back;
+                head = head.next;
+            }
+
+            return front == back;
+        }
+
+        //參考 : https://skyyen999.gitbooks.io/-leetcode-with-javascript/content/questions/234md.html
+        //找出中點,反轉後面list,比對前後list
+
+        public bool IsPalindromeEx2(ListNode head)
+        {
+            ListNode midPoint = FindMiddle(head);
+            ListNode rNode = ReverseList(midPoint); // 從中點反轉
+
+            while (rNode != null)
+            {
+                if (head.val != rNode.val)
+                {
+                    return false;
+                }
+                head = head.next;
+                rNode = rNode.next;
+            }
+            return true;
+
+        }
+
+        // 使用快慢指針找出中點
+        public ListNode FindMiddle(ListNode node)
+        {
+            ListNode fast = node;
+            ListNode slow = node;
+
+            while (fast != null && fast.next != null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            return slow;
+        }
+
+        //反轉
+        public ListNode ReverseList(ListNode node)
+        {
+            if (node == null || node.next == null) return node;
+            ListNode prev = null;
+            ListNode cur = node;
+            ListNode temp = cur;
+            while (cur != null)
+            {
+                temp = cur;
+                cur = cur.next;
+                temp.next = prev;
+                prev = temp;
+            }
+            return prev;
+        }
+
+        /**
+        * Definition for singly-linked list.
+        * public class ListNode {
+        *     public int val;
+        *     public ListNode next;
+        *     public ListNode(int val=0, ListNode next=null) {
+        *         this.val = val;
+        *         this.next = next;
+        *     }
+        * }
+        */
+
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/LeetcodeExample/ReverseLinkedList.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1.LeetcodeExample
+{
+    class ReverseLinkedList
+    {
+        //https://ithelp.ithome.com.tw/articles/10271920
+        //將link指向反過來
+        public ListNode ReverseList(ListNode head)
+        {
+            ListNode next = null;
+            ListNode curr = head;
+            ListNode pre = null;
+
+            while (next != null)
+            {
+                next = curr.next;
+                curr.next = pre;
+                pre = curr;
+                curr = next;
+            }
+
+            return pre;
+        }
+
+
+        /**
+        * Definition for singly-linked list.
+        * public class ListNode {
+        *     public int val;
+        *     public ListNode next;
+        *     public ListNode(int val=0, ListNode next=null) {
+        *         this.val = val;
+        *         this.next = next;
+        *     }
+        * }
+        */
+
+    }
+}
